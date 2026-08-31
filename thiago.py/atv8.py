5c6f80684684785bfe71d2d00f9ff2e5938b171f
atvifpi <ifpisul0@gmail.com>
2026-08-31 10:40:37 -0300

    adsadsadsadasdsadsadsad

--- thiago.py/atv8.py
+++
@@ -1,6 +0,0 @@
-numero = int(input("Digite um número inteiro: "))
-
-if numero % 5 == 0:
-    print("O número é múltiplo de 5")
-else:
-    print("O número não é múltiplo de 5")
--- thiago.py/atv9.py
+++
@@ -1,8 +0,0 @@
-quantidade = int(input("Digite a quantidade atual em stock: "))
-
-if quantidade > 100:
-    print("Stock elevado")
-elif quantidade >= 20:
-    print("Stock adequado")
-else:
-    print("Atenção: Stock crítico! Reabastecer agora")
